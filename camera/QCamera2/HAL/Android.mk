a6edd3b8d3eac6d0d2c9ffc1f94dda08c9f9ed6b
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-03-19 01:01:26 +0800

    ferrari: camera: Waits for sensorservice initialization before opening inside the actual HAL
    * Moved to OSS HAL from dropped CameraWrapper
    
    Change-Id: I876d2f0a57e17da392d451987047189b05a5e72b
    (cherry picked from commit b15c3f514f8e045952b6112e4a3a1e0c39a1f8d5)

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -71,7 +71,7 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl liblog
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl liblog libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
 LOCAL_SHARED_LIBRARIES += libhidltransport libsensor android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
 LOCAL_STATIC_LIBRARIES := libarect
