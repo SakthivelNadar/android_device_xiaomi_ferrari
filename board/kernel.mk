86f719fccd1d62b49429bc86d7e1b2fe48b418ec
thewisenerd <thewisenerd@protonmail.com>
2017-10-20 22:52:00 +0530

    ferrari: Add kernel flags

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,7 @@
 # Kernel
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_USES_UNCOMPRESSED_KERNEL := true
 TARGET_KERNEL_CONFIG := ferrari_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ferrari
 
