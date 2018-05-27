5747c48b24a599781f7aa67d9ad017238e35a7c2
thewisenerd <thewisenerd@protonmail.com>
2018-05-27 10:19:32 +0530

    fix kernel compile, boot permissive
    Change-Id: I9b9a4d765a7cf24b78743527c34053e4d156c49c
    (cherry picked from commit 5ef13c48a5fa6912e35ed2de1a99930d209fa0ee)

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,7 @@
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_SEPARATED_DT := true
-TARGET_USES_UNCOMPRESSED_KERNEL := true
+BOARD_KERNEL_IMAGE_NAME := Image
 
 ifeq ($(WITH_TWRP),true)
 TARGET_KERNEL_CONFIG := ferrari-recovery_defconfig
@@ -10,3 +10,4 @@ TARGET_KERNEL_CONFIG := ferrari_defconfig
 endif
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ferrari
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
