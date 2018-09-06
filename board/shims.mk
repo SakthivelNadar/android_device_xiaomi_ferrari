72ce0d144dd3f3cae6a5f4b31d01046700a561b6
thewisenerd <thewisenerd@protonmail.com>
2018-09-06 07:37:18 +0530

    add camera shims
    Change-Id: I81325f0e7642eb30d31f8b082accd55bf58bf3ec
    (cherry picked from commit ceac16d401a276a1c2125b748f565487fe3dfa57)

--- board/shims.mk
+++ board/shims.mk
@@ -1,3 +1,6 @@
 TARGET_LD_SHIM_LIBS += \
     /system/bin/mm-pp-daemon|libcamera_shim.so \
-    /vendor/bin/perfd|libcamera_shim.so
+    /vendor/bin/perfd|libcamera_shim.so \
+    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib/libmmcamera2_imglib_modules.so|libcamera_shim.so \
+    /system/lib/libmmjpeg_interface.so|libshim_atomic.so
