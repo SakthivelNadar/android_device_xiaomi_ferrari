7ad8254036c06d5b01644bfdc8b0933a0c8b1f49
thewisenerd <thewisenerd@protonmail.com>
2018-09-07 08:59:40 +0530

    move permissions to vendor, nuke unused sensors/hal.conf
    Change-Id: I80a52f902788a51e5fc70889b38f5a7132a748e8
    (cherry picked from commit 41b1cb4ea27478ff124505b54c57670140fa639c)

--- product/sensors.mk
+++ product/sensors.mk
@@ -3,15 +3,11 @@ PRODUCT_PACKAGES += \
     sensors.msm8916 \
     android.hardware.sensors@1.0-impl
 
-# Multi HAL configuration file
-PRODUCT_COPY_FILES += \
-    device/xiaomi/ferrari/sensors/etc/hals.conf:vendor/etc/sensors/hals.conf
-
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
-    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml
+    $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/vendor/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/vendor/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/vendor/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/vendor/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/vendor/etc/permissions/android.hardware.sensor.stepcounter.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/vendor/etc/permissions/android.hardware.sensor.stepdetector.xml
