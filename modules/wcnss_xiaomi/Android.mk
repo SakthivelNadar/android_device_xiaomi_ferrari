9f3f18ff91ac9adb550176e6e335c5caa1b62d9b
thewisenerd <thewisenerd@protonmail.com>
2018-09-23 02:34:19 +0530

    move oss wcnss_qmi to vendor/

--- modules/wcnss_xiaomi/Android.mk
+++ modules/wcnss_xiaomi/Android.mk
@@ -1,13 +1,11 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := wcnss_xiaomi_client.c
-LOCAL_C_INCLUDES += hardware/qcom/wlan/wcnss_service
-LOCAL_CFLAGS += -Wall
-
-LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := libwcnss_qmi
-
+LOCAL_MODULE             := libwcnss_qmi
+LOCAL_MODULE_TAGS        := optional
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_SRC_FILES          := wcnss_xiaomi_client.c
+LOCAL_CFLAGS             += -Wall
+LOCAL_C_INCLUDES         += hardware/qcom/wlan/wcnss_service
+LOCAL_SHARED_LIBRARIES   := libc libcutils libutils liblog
 include $(BUILD_SHARED_LIBRARY)
