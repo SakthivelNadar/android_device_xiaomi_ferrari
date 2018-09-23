9893564279de954be85e35c69f5b39433a20f943
thewisenerd <thewisenerd@protonmail.com>
2018-09-23 13:38:00 +0530

    ferrari: update build fingerprint

--- lineage.mk
+++ lineage.mk
@@ -31,7 +31,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="ferrari-user 5.0.2 LRX22G V8.1.5.0.LXIMIDI release-keys" \
+	PRIVATE_BUILD_DESC="ferrari-user 5.0.2 LRX22G V9.2.3.0.LXIMIEK release-keys" \
 	PRODUCT_NAME="ferrari"
 
-BUILD_FINGERPRINT := "Xiaomi/ferrari/ferrari:5.0.2/LRX22G/V8.1.5.0.LXIMIDI:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/ferrari/ferrari:5.0.2/LRX22G/V9.2.3.0.LXIMIEK:user/release-keys"
