dfbe7fe559ecd792d03352366ed26d4b945338f7
thewisenerd <thewisenerd@protonmail.com>
2018-09-23 07:46:24 +0530

    Revert "ferrari: QCamera2: Disable CLANG"
    This reverts commit 0e7769df7e812d7cce3639f11729e22587fd559b.

--- camera/QCamera2/stack/mm-camera-test/Android.mk
+++ camera/QCamera2/stack/mm-camera-test/Android.mk
@@ -103,7 +103,6 @@ LOCAL_SHARED_LIBRARIES:= \
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE:= mm-qcamera-app
-LOCAL_CLANG := false
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_EXECUTABLE)
 endif
@@ -210,7 +209,6 @@ LOCAL_SHARED_LIBRARIES:= \
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE:= libmm-qcamera
-LOCAL_CLANG := false
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
 endif
