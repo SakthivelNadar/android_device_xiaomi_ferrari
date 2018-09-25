66ffc68c2431b2a984368f75d79c24aa1150a4c6
thewisenerd <thewisenerd@protonmail.com>
2018-09-26 03:08:51 +0530

    twrp: changes for encryption
    * [revertlater] nuke qseecomd, and use system vold instead
      twrp decryption broken atm (or I can't be bothered to fix it yet)
      working /system partition (oreo?) required for decryption to work
    
    * nuke TARGET_KEYMASTER_WAIT_FOR_QSEE := false (we need qsee)
    * use toybox instead of busybox
    * rename bk1 label to something relevant
    * move .qcom.rc to .twrp.rc (we have other files in common tree)
    * add logd, and logcat
    * update tzdata path
    
    Change-Id: Ieaa5730efa5a37f84b0259cd451f402769e92cd3

--- twrp.mk
+++ twrp.mk
@@ -1,5 +1,6 @@
 TARGET_RECOVERY_DEVICE_DIRS += device/xiaomi/ferrari/twrp
 TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd hwservicemanager keymaster-3-0
 TW_THEME := portrait_hdpi
 
 RECOVERY_SDCARD_ON_DATA := true
@@ -11,7 +12,11 @@ TW_MTP_DEVICE := "/dev/mtp_usb"
 TW_NO_USB_STORAGE := true
 TW_OEM_BUILD := false
 TW_EXCLUDE_TWRPAPP := true
+TW_USE_TOOLBOX := true
+
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
 
 # Time Zone data for TWRP
 PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+    system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
