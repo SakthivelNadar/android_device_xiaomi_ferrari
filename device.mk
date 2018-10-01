968be5f0e7b05f2a9c89ff8cb483b404c775e725
thewisenerd <thewisenerd@protonmail.com>
2018-10-02 04:33:14 +0530

    ferrari: move thermal conf back to /system
    thermal-engine was missing these dearly
    
    Change-Id: I549655094884a593f3cf964f23f1402ca8228d81

--- device.mk
+++ device.mk
@@ -123,8 +123,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8939.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8939.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-8939.IN.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8939.IN.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8939.conf:system/etc/thermal-engine-8939.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-8939.IN.conf:system/etc/thermal-engine-8939.IN.conf
 
 # USB VID
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
